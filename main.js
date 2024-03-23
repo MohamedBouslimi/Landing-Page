fe11bfa090b1314208356f8fe40cce3dfdd05fac
MohamedBouslimi <106472283+MohamedBouslimi@users.noreply.github.com>
2024-03-23 22:20:00 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+let img = document.querySelector(".img");  
+let container = document.querySelector(".container");
+function phone(x){ 
+    img.src=x;
+}  
+
+function colors(color){  
+    container.style.background=color;
+    
+}
